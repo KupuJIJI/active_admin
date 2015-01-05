7b0be586976f2256ddd02e1394629efce7d08be8
KupuJIJI <kirill.dmitrievich24@yandex.ru>
2015-01-05 12:24:56 +0200

    permit params

---
+++ app/controllers/activeadmin_settings/settings_controller.rb
@@ -0,0 +1,21 @@
+class ActiveadminSettings::SettingsController < ApplicationController
+  before_filter :authenticate_admin_user!
+
+  def update
+    @object = ActiveadminSettings::Setting.find(params[:id])
+    if @object.update_attributes(permitted_params[:setting])
+      render :text => @object.value
+    else
+      render :text => "error"
+    end
+  end
+
+  # Define the permitted params in case the app is using Strong Parameters
+  def permitted_params
+    if Rails::VERSION::MAJOR == 3 && !defined? StrongParameters
+      params
+    else
+      params.permit setting: [:name, :string, :file, :remove_file, :locale]
+    end
+  end 
+end
